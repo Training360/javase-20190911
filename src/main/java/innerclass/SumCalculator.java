eacfa03b2af91ac01bd23b86a8a58197308cf487
vicziani <viczian.istvan@gmail.com>
2019-09-11 12:02:20 +0200

    Megoldás

--- src/main/java/innerclass/SumCalculator.java
+++ src/main/java/innerclass/SumCalculator.java
@@ -1,13 +1,30 @@
 package innerclass;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SumCalculator {
 
     public int calculateSum(Bill bill, TaxCalculator taxCalculator) {
         int sum = 0;
         for (BillItem item: bill.getBillItems()) {
             int base = item.getAmount() * item.getPrice();
-            sum += base * taxCalculator.calculateTax(base);
+            sum += base + taxCalculator.calculateTax(base);
         }
         return sum;
+    }
+
+    public static void main(String[] args) {
+        List<BillItem> items = Arrays.asList(new BillItem(10, 200),
+                new BillItem(20, 300));
+        Bill bill = new Bill(items);
+
+        int sum = new SumCalculator().calculateSum(bill, new TaxCalculator() {
+            @Override
+            public int calculateTax(int base) {
+                return (int)(0.2 * base);
+            }
+        });
+        System.out.println(sum);
     }
 }
