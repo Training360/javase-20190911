1039bae49fe3277dca68fb201448374c6626fab3
vicziani <viczian.istvan@gmail.com>
2019-09-13 11:07:37 +0200

    File iras

---
+++ src/main/java/trainers/TrainersWriter.java
@@ -0,0 +1,22 @@
+package trainers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class TrainersWriter {
+
+    public void writeTrainers(List<String> trainers, Path file) {
+        try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(file))) {
+            for (String trainer: trainers) {
+                writer.println(trainer);
+            }
+        }
+        catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file", ioe);
+        }
+    }
+}
